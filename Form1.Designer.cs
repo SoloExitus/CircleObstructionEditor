f46787774ef73821cc673148f269e1a5561c2330
Exitus <62836994+SoloExitus@users.noreply.github.com>
2022-01-29 09:38:18 +0300

    Upload v1.0.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace CircleEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.newbutton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rb_Mode_Remove = new System.Windows.Forms.RadioButton();
+            this.rb_Mode_Create = new System.Windows.Forms.RadioButton();
+            this.rb_Mode_None = new System.Windows.Forms.RadioButton();
+            this.rb_Mode_Edit = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // newbutton
+            // 
+            this.newbutton.Location = new System.Drawing.Point(6, 19);
+            this.newbutton.Name = "newbutton";
+            this.newbutton.Size = new System.Drawing.Size(69, 21);
+            this.newbutton.TabIndex = 0;
+            this.newbutton.Text = "New";
+            this.newbutton.UseVisualStyleBackColor = true;
+            this.newbutton.Click += new System.EventHandler(this.Newbutton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(624, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 190);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.newbutton);
+            this.groupBox3.Location = new System.Drawing.Point(11, 94);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(147, 79);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Map";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(81, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Loadbutton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rb_Mode_Remove);
+            this.groupBox2.Controls.Add(this.rb_Mode_Create);
+            this.groupBox2.Controls.Add(this.rb_Mode_None);
+            this.groupBox2.Controls.Add(this.rb_Mode_Edit);
+            this.groupBox2.Location = new System.Drawing.Point(11, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(147, 69);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mode";
+            // 
+            // rb_Mode_Remove
+            // 
+            this.rb_Mode_Remove.AutoSize = true;
+            this.rb_Mode_Remove.Location = new System.Drawing.Point(7, 42);
+            this.rb_Mode_Remove.Name = "rb_Mode_Remove";
+            this.rb_Mode_Remove.Size = new System.Drawing.Size(65, 17);
+            this.rb_Mode_Remove.TabIndex = 4;
+            this.rb_Mode_Remove.TabStop = true;
+            this.rb_Mode_Remove.Text = "Remove";
+            this.rb_Mode_Remove.UseVisualStyleBackColor = true;
+            this.rb_Mode_Remove.CheckedChanged += new System.EventHandler(this.rb_Mode_Remove_CheckedChanged);
+            // 
+            // rb_Mode_Create
+            // 
+            this.rb_Mode_Create.AutoSize = true;
+            this.rb_Mode_Create.Location = new System.Drawing.Point(76, 19);
+            this.rb_Mode_Create.Name = "rb_Mode_Create";
+            this.rb_Mode_Create.Size = new System.Drawing.Size(56, 17);
+            this.rb_Mode_Create.TabIndex = 3;
+            this.rb_Mode_Create.TabStop = true;
+            this.rb_Mode_Create.Text = "Create";
+            this.rb_Mode_Create.UseVisualStyleBackColor = true;
+            this.rb_Mode_Create.CheckedChanged += new System.EventHandler(this.rb_Mode_Create_CheckedChanged);
+            // 
+            // rb_Mode_None
+            // 
+            this.rb_Mode_None.AutoSize = true;
+            this.rb_Mode_None.Location = new System.Drawing.Point(6, 19);
+            this.rb_Mode_None.Name = "rb_Mode_None";
+            this.rb_Mode_None.Size = new System.Drawing.Size(51, 17);
+            this.rb_Mode_None.TabIndex = 1;
+            this.rb_Mode_None.TabStop = true;
+            this.rb_Mode_None.Text = "None";
+            this.rb_Mode_None.UseVisualStyleBackColor = true;
+            this.rb_Mode_None.CheckedChanged += new System.EventHandler(this.rb_Mode_None_CheckedChanged);
+            // 
+            // rb_Mode_Edit
+            // 
+            this.rb_Mode_Edit.AutoSize = true;
+            this.rb_Mode_Edit.Location = new System.Drawing.Point(76, 42);
+            this.rb_Mode_Edit.Name = "rb_Mode_Edit";
+            this.rb_Mode_Edit.Size = new System.Drawing.Size(43, 17);
+            this.rb_Mode_Edit.TabIndex = 2;
+            this.rb_Mode_Edit.TabStop = true;
+            this.rb_Mode_Edit.Text = "Edit";
+            this.rb_Mode_Edit.UseVisualStyleBackColor = true;
+            this.rb_Mode_Edit.CheckedChanged += new System.EventHandler(this.rb_Mode_Edit_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ModePoint_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ModePoint_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newbutton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rb_Mode_Create;
+        private System.Windows.Forms.RadioButton rb_Mode_None;
+        private System.Windows.Forms.RadioButton rb_Mode_Edit;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton rb_Mode_Remove;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml.Linq;
+
+namespace CircleEditor
+{
+
+    public partial class Form1 : Form
+    {
+
+        enum Mode
+        {
+            None = 0,
+            Create,
+            Creating,
+            Edit,
+            Moving,
+            Editing,
+            Remove
+        }
+
+
+        class Circle
+        {
+
+            public PointF m_center;
+            public float m_radius = 0;
+
+            public Circle()
+            {
+                m_center = new PointF();
+            }
+
+            public Circle(PointF center)
+            {
+                m_center = center;
+            }
+
+            public Circle(PointF center, float radius)
+            {
+                m_center = center;
+                m_radius = radius;
+            }
+
+            public void setRadius(float radius)
+            {
+                m_radius = radius;
+            }
+
+            private float distance(PointF a, PointF b)
+            {
+                float dX = a.X - b.X;
+                float dY = a.Y - b.Y;
+                return (float)Math.Sqrt((double)(dX * dX + dY * dY));
+            }
+
+            public void setRadius(PointF e)
+            {
+                m_radius = distance(m_center, e);
+            }
+
+            public void setCenter(PointF center)
+            {
+                m_center = center;
+            }
+
+
+        }
+
+
+
+        Mode m_currentMode = Mode.None;
+
+        Graphics G;
+        bool m_isUpdate = false;
+
+        List<Circle> m_Obstructions;
+
+        int m_creatingIndex = -1;
+        int m_editingIndex = -1;
+
+        System.Drawing.SolidBrush m_obstructionsBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Blue);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_currentMode = Mode.None;
+
+            openFileDialog1.Filter = "Text files(*.xml)|*.xml|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.xml)|*.xml|All files(*.*)|*.*";
+
+            G = CreateGraphics();
+
+            m_Obstructions = new List<Circle>();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (m_isUpdate)
+            {
+                G.Clear(Color.White);
+                Draw();
+                m_isUpdate = false;
+            }
+        }
+
+        private void Draw()
+        {
+            foreach (Circle obstruction in m_Obstructions)
+            {
+                PointF center = obstruction.m_center;
+                float radius = obstruction.m_radius;
+                G.FillEllipse(m_obstructionsBrush, center.X - radius, center.Y - radius, radius * 2, radius * 2);
+            }
+        }
+
+        private void rb_Mode_Remove_CheckedChanged(object sender, EventArgs e)
+        {
+            m_currentMode = Mode.Remove;
+        }
+
+        private void rb_Mode_Edit_CheckedChanged(object sender, EventArgs e)
+        {
+            m_currentMode = Mode.Edit;
+        }
+
+        private void rb_Mode_None_CheckedChanged(object sender, EventArgs e)
+        {
+            m_currentMode = Mode.None;
+        }
+
+        private void rb_Mode_Create_CheckedChanged(object sender, EventArgs e)
+        {
+            m_currentMode = Mode.Create;
+        }
+
+        private float distance(PointF a, PointF b)
+        {
+            float dX = a.X - b.X;
+            float dY = a.Y - b.Y;
+            return (float)Math.Sqrt((double)(dX * dX + dY * dY));
+        }
+
+        private int TryGrabObstruction(PointF location)
+        {
+            int count = m_Obstructions.Count();
+
+            int index = -1;
+            float minDistToObstruction = float.MaxValue;
+
+            for (int i = 0; i < count; i++)
+            {
+                float dist = distance(m_Obstructions[i].m_center, location);
+                if (dist < minDistToObstruction && dist < m_Obstructions[i].m_radius)
+                {
+                    minDistToObstruction = dist;
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        private void ModePoint_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (m_currentMode)
+            {
+                case Mode.Creating:
+                    m_Obstructions[m_creatingIndex].setRadius(new PointF(e.Location.X, e.Location.Y));
+                    m_isUpdate = true;
+                    break;
+                case Mode.Moving:
+                    m_Obstructions[m_editingIndex].setCenter(new PointF(e.Location.X, e.Location.Y));
+                    m_isUpdate = true;
+                    break;
+                case Mode.Editing:
+                    m_Obstructions[m_editingIndex].setRadius(new PointF(e.Location.X, e.Location.Y));
+                    m_isUpdate = true;
+                    break;
+
+            }
+        }
+
+        private void ModePoint_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                switch (m_currentMode)
+                {
+                    case Mode.Create:
+                        m_creatingIndex = m_Obstructions.Count();
+                        m_Obstructions.Add(new Circle(new PointF(e.Location.X, e.Location.Y)));
+                        m_currentMode = Mode.Creating;
+                        break;
+                    case Mode.Creating:
+                        m_Obstructions[m_creatingIndex].setRadius(new PointF(e.Location.X, e.Location.Y));
+                        m_creatingIndex = -1;
+                        m_currentMode = Mode.Create;
+                        m_isUpdate = true;
+                        break;
+                    case Mode.Remove:
+                        int removeIndex = TryGrabObstruction(new PointF(e.Location.X, e.Location.Y));
+                        if (removeIndex > -1)
+                        {
+                            m_Obstructions.Remove(m_Obstructions[removeIndex]);
+                        }
+                        m_isUpdate = true;
+                        break;
+                    case Mode.Edit:
+                        m_editingIndex = TryGrabObstruction(new PointF(e.Location.X, e.Location.Y));
+                        if (m_editingIndex > -1)
+                        {
+                            m_currentMode = Mode.Moving;
+                        }
+
+                        break;
+                    case Mode.Moving:
+                        m_Obstructions[m_editingIndex].setCenter(new PointF(e.Location.X, e.Location.Y));
+                        m_editingIndex = -1;
+                        m_currentMode = Mode.Edit;
+                        m_isUpdate = true;
+                        break;
+
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                switch (m_currentMode)
+                {
+                    case Mode.Edit:
+                        m_editingIndex = TryGrabObstruction(new PointF(e.Location.X, e.Location.Y));
+                        if (m_editingIndex > -1)
+                        {
+                            m_currentMode = Mode.Editing;
+                        }
+                        break;
+                    case Mode.Editing:
+                        m_Obstructions[m_editingIndex].setRadius(new PointF(e.Location.X, e.Location.Y));
+                        m_currentMode = Mode.Edit;
+                        m_isUpdate = true;
+                        break;
+
+
+                }
+
+            }
+        }
+
+        private void Newbutton_Click(object sender, EventArgs e)
+        {
+            m_Obstructions.Clear();
+            m_isUpdate = true;
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = saveFileDialog1.FileName;
+
+            XDocument map = new XDocument();
+            
+            XElement obstructions = new XElement("obstructions");
+
+            foreach(Circle obstruct in m_Obstructions)
+            {
+                XElement obstruction = new XElement("obstruction");
+
+                XElement centerPointElement = new XElement("center");
+                XElement radiusElement = new XElement("radius");
+
+                XAttribute centerX = new XAttribute("X", obstruct.m_center.X.ToString());
+                XAttribute centerY = new XAttribute("Y", obstruct.m_center.Y.ToString());
+
+                XAttribute radius = new XAttribute("radius", obstruct.m_radius);
+
+                centerPointElement.Add(centerX);
+                centerPointElement.Add(centerY);
+
+                radiusElement.Add(radius);
+
+                obstruction.Add(centerPointElement);
+                obstruction.Add(radiusElement);
+
+                obstructions.Add(obstruction);
+            }
+
+            obstructions.Save(filename);
+        }
+
+        private void Loadbutton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = openFileDialog1.FileName;
+            XDocument xdoc = XDocument.Load(filename);
+
+            m_Obstructions.Clear();
+
+            foreach (XElement obstructionElement in xdoc.Element("obstructions").Elements("obstruction"))
+            {
+                XElement centerPointElement = obstructionElement.Element("center");
+                XElement radiusElement = obstructionElement.Element("radius");
+
+                XAttribute centerX = centerPointElement.Attribute("X");
+                XAttribute centerY = centerPointElement.Attribute("Y");
+                XAttribute radius = radiusElement.Attribute("radius");
+
+                if (centerX != null && centerY != null && radius != null)
+                {
+                    PointF c = new PointF((float)centerX, (float)centerY);
+                    float r = float.Parse(radius.Value);
+                    //float r = float.Parse(radius.Value);
+                    Circle obstruction = new Circle(c, r);
+                    m_Obstructions.Add(obstruction);
+                }
+            }
+
+            m_isUpdate = true;
+        }
+    }
+}
+
